3c6ec0eae5c8a34ab32f195a5568a89e26a0eb52
Sai-Keerthi338 <saikeerthijada@gmail.com>
2025-05-24 09:11:07 +0530

    sharpening image and detecting edges

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from preprocessing.preprocess import load_image, preprocess_image
+from preprocessing.preprocess import load_image, preprocess_image, sharpen_image, detect_edges
 import matplotlib.pyplot as plt
 import cv2
 
@@ -6,17 +6,30 @@ def main():
     image_path="data/parking_zone.jpg"
     image=load_image(image_path)
     preprocessed=preprocess_image(image)
+
+    sharpened=sharpen_image(preprocessed) 
+    edges=detect_edges(preprocessed)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(15, 8))
 
-    plt.subplot(1, 2, 1)
+    plt.subplot(2, 2, 1)
     plt.title("Original Image")
     plt.imshow(image_rgb)
     plt.axis("off")
 
-    plt.subplot(1, 2, 2)
+    plt.subplot(2, 2, 2)
     plt.title("Preprocessed Image (Grayscale + Blur)")
+    plt.imshow(preprocessed, cmap='gray')
+    plt.axis("off")
+
+    plt.subplot(2, 2, 3)
+    plt.title("Sharpened Image")
+    plt.imshow(preprocessed, cmap='gray')
+    plt.axis("off")
+
+    plt.subplot(2, 2, 4)
+    plt.title("Canny edges")
     plt.imshow(preprocessed, cmap='gray')
     plt.axis("off")
 
--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+
 def load_image(image_path):
     image=cv2.imread(image_path) # type: ignore
     if image is None:
@@ -8,3 +10,15 @@ def preprocess_image(image):
     gray=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blur=cv2.GaussianBlur(gray, (5,5), 0)
     return blur
+def sharpen_image(image):
+    kernel = np.array([[0, -1, 0],
+                       [-1, 5,-1],
+                       [0, -1, 0]])
+    # Apply filter2D to sharpen the image using the kernel
+    sharpened = cv2.filter2D(image, -1, kernel)
+    return sharpened
+
+def detect_edges(image):
+    # Use Canny edge detection on the grayscale image
+    edges = cv2.Canny(image, threshold1=50, threshold2=150)
+    return edges
